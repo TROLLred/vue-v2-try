971616ed397448b09bfa7a65e37871b337dd0869
TROLLred <61773682+TROLLred-IX@users.noreply.github.com>
2022-06-05 05:11:08 +0700

    Vue suck

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container fluid>
+      <v-app-bar
+        color="deep-purple accent-4"
+        dark
+      >
+        <v-spacer/>
+        <v-btn class="ml-3">
+            <router-link to="/">Главная</router-link>
+        </v-btn>
+        <v-btn class="ml-3">
+            <router-link to="/photo">Фотографии</router-link>
+        </v-btn>
+      </v-app-bar>
+    </v-container>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    
+</style>
